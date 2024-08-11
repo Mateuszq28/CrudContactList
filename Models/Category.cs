acbe9c05e6c46ac6a593d4f2bb528ced9894ee56
mateusz_pikus <mateuszq28@gmail.com>
2024-08-11 18:40:54 +0200

    category and subcategory model class .cs

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudContactList.Models
+{
+    public class Category
+    {
+        [KeyAttribute]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category()
+        {
+            
+        }
+    }
+}
---
+++ Models/Subcategory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudContactList.Models
+{
+    public class Subcategory
+    {
+        [KeyAttribute]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Subcategory()
+        {
+            
+        }
+    }
+}
