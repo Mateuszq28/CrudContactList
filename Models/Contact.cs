50b9aac327342afe788bdb06fd0f4b89535122cb
mateusz_pikus <mateuszq28@gmail.com>
2024-08-11 17:29:51 +0200

    Contact class with comments

---
+++ Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudContactList.Models
+{
+    public class Contact
+    {
+        // an email could also work as an Id
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        // unique
+        public string email { get; set; }
+        // strong password
+        public string password { get; set; }
+        // unique
+        // w - combo box
+        public string category { get; set; }
+        // optional
+        // w - combo box or text field
+        public string? subcategory { get; set; }
+        // regular extension
+        public string phone { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime birthDate { get; set; }
+    }
+}
