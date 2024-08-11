6b9c25d6bbfeb7a7dccd53f97a44dfad77a2a2f6
mateusz_pikus <mateuszq28@gmail.com>
2024-08-11 19:39:41 +0200

    email as a key

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -4,9 +4,6 @@ namespace CrudContactList.Models
 {
     public class Contact
     {
-        // an email could also work as an Id
-        [KeyAttribute]
-        public int Id { get; set; }
         public string name { get; set; }
         public string surname { get; set; }
         // unique
