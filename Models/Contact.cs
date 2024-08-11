8c8ca46e71fc8cbe2b9d235ee5924d5df7fc5190
mateusz_pikus <mateuszq28@gmail.com>
2024-08-11 18:36:30 +0200

    decorators in Contact model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -5,22 +5,30 @@ namespace CrudContactList.Models
     public class Contact
     {
         // an email could also work as an Id
+        [KeyAttribute]
         public int Id { get; set; }
         public string name { get; set; }
         public string surname { get; set; }
         // unique
+        [KeyAttribute]
         public string email { get; set; }
         // strong password
         public string password { get; set; }
-        // unique
         // w - combo box
         public string category { get; set; }
         // optional
         // w - combo box or text field
         public string? subcategory { get; set; }
-        // regular extension
+        // regular expression
+        [RegularExpression(@"^\d{3}-\d{3}-\d{3}$")]
         public string phone { get; set; }
+        // only date
         [DataType(DataType.Date)]
         public DateTime birthDate { get; set; }
+
+        public Contact()
+        {
+
+        }
     }
 }
