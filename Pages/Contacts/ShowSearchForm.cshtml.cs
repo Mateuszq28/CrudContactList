bcdf75bd6325d2937dabd9d65b179ef840ae194d
mateusz_pikus <mateuszq28@gmail.com>
2024-08-13 01:10:21 +0200

    stuck on form

--- Pages/Contacts/ShowSearchForm.cshtml.cs
+++ Pages/Contacts/ShowSearchForm.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using CrudContactList.Data;
 using CrudContactList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudContactList.Pages.Contacts
 {
@@ -28,6 +29,7 @@ namespace CrudContactList.Pages.Contacts
         public Contact Contact { get; set; } = default!;
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        /*
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
@@ -39,6 +41,16 @@ namespace CrudContactList.Pages.Contacts
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+        */
+
+        // PoST: Contacts/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(string SearchPhrase)
+        {
+            // Console.WriteLine won't show in the output. But using red debuging dots shows, that this function is never called up.
+            Console.WriteLine("cont controller ShowSearchResults");
+            await _context.Contact.Where(j => j.Name.Contains(SearchPhrase)).ToListAsync();
+            return Page();
         }
     }
 }
